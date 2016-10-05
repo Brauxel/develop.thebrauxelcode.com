f0f9f60adc93391f13613676a534ddbfb38d1c1d
Aakash Bhatia <aakash@stocksdigital.com>
2016-10-05 14:28:56 +1100

    sms-template

---
+++ wp-content/themes/thebrauxelcode/sms-test.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: SMS Test
+*/
+?>
+<?php get_header(); ?>
+    <main>
+        <div class="wrap">
+            <h1><?php the_title(); ?></h1>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	            <?php the_content(); ?>
+            <?php endwhile; else: ?>
+	            <p>No content!</p>
+            <?php endif; ?>
+        <!-- div.wrap ENDS -->
+        </div>
+    </main>
+<?php get_footer(); ?>
